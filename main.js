d19ea478e4e4a366a412bf1097af67f484c885d3
praconfi <praconfi@gmail.com>
2020-08-30 20:01:45 +0900

    ➕JS:navbar

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// 주석색을 확ㅇ니한다.\
+'use strict';
+//  Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark')
+    } else {
+        navbar.classList.remove('navbar--dark')
+    }
+});
